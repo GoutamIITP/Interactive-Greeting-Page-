05670739f117cad4858e269d00e57b37372028c3
GoutamIITP <goutamsah126@gmail.com>
2025-03-08 12:00:04 +0530

    change in Greeting hiddeb

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ function greetUser() {
     let color = prompt("What is your favorite color?");
 
     if (name && color) {
-        alert(`Hello, ${name}! Your favorite color is ${color}.`);
         document.body.style.backgroundColor = color;
+        let greetingBox = document.getElementById('greeting-box');
+        let greetingMessage = document.getElementById('greeting-message');
+
+        greetingMessage.innerText = `Welcome to the page, ${name}!`;
+        greetingBox.style.backgroundColor = color;
+        greetingBox.classList.remove('hidden');
     }else {
         alert('Please provide a name and a color.');
     }
